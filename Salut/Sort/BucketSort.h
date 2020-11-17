671579fd7a7595c8af748e6d9d639cfba6126154
zzb <azmddy@163.com>
2020-11-17 13:59:06 +0000

    updaet

--- Salut/Sort/BucketSort.h
+++ Salut/Sort/BucketSort.h
@@ -5,6 +5,7 @@
 #include <functional>
 namespace salut {
     namespace sort {
+        // arr: 待排序数组；bucketNum：桶的数量；mappingFunc映射函数
         void BucketSort(std::vector<int>& arr, int bucketNum, const std::function<int(int)>& mappingFunc);
     }  // namespace sort
 }  // namespace salut
