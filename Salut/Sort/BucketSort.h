efe223c66f7610f54176e45965709e4cbfef45ee
zzb <azmddy@163.com>
2020-11-28 20:33:43 +0800

    update

--- Salut/Sort/BucketSort.h
+++ Salut/Sort/BucketSort.h
@@ -2,11 +2,28 @@
 #define SALUT_SORT_BUCKET_SORT_H_
 #include <algorithm>
 #include <vector>
+#include <unordered_map>
 #include <functional>
 namespace salut {
     namespace sort {
-        // arr: 待排序数组；bucketNum：桶的数量；mappingFunc映射函数
-        void BucketSort(std::vector<int>& arr, int bucketNum, const std::function<int(int)>& mappingFunc);
+        // arr: 待排序数组；bucketNum：桶的数量；mappingFunc映射函数；comp: 桶内排序用的比较函数
+        template<class T>
+        void BucketSort(std::vector<T>& arr,
+                        uint32_t bucketNum,
+                        const std::function<uint32_t(T)>& mappingFunc,
+                        const std::function<bool(const T& lhs, const T& rhs)>& comp = std::less<T>())
+        {
+            std::vector<std::vector<T>> buckets(bucketNum);
+            for (auto& a : arr) {
+                buckets[mappingFunc(a)].push_back(a);
+            }
+            for (uint32_t i = 0, j = 0; i < bucketNum; i++) {
+                std::sort(buckets[i].begin(), buckets[i].end(), comp);
+                for (auto& v : buckets[i]) {
+                    arr[j++] = v;
+                }
+            }
+        }
     }  // namespace sort
 }  // namespace salut
 #endif  // SALUT_SORT_BUCKET_SORT_H_
--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ using namespace salut;
 int main(int argc, char** argv)
 {
     vector<int> arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    sort::BucketSort(arr, 5, [](int a) { return a / 2; });
+    sort::BucketSort<int>(
+        arr, 5, [](int a) { return 4 - a / 2; });
     for (auto& a : arr) {
         std::cout << a << ",";
     }
